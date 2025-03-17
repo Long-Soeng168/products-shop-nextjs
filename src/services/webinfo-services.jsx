af27173b4cedbc10d58dfdbf7101bd2ea2791505
Long-Soeng168 <generalpurpose167@gmail.com>
2025-03-17 10:01:48 +0700

    update

---
+++ src/services/webinfo-services.jsx
@@ -0,0 +1,19 @@
+import { BASE_API_URL } from "@/config/env";
+
+export async function getwebinfo() {
+  const url = BASE_API_URL + `/footer`;
+  try {
+    const response = await fetch(url, {
+      next: {
+        revalidate: 3600,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Failed to fetch Footer : ${response.statusText}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
